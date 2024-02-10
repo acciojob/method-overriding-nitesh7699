e5f12cdd7ff215cc24e8ab5a65beaab85019b02f
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-10 19:04:36 +0530

    changes4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,19 @@
 package com.driver;
 
-class A{
-    public String meth(String str){
-        return str;
-    }
-}
-
-class B extends A{
-    public String meth(String str1){
-        return str1;
-    }
-}
-
 public class Main {
+
+    static class A{
+        public String meth(String str){
+            return str;
+        }
+    }
+    
+    static class B extends A{
+        public String meth(String str1){
+            return str1;
+        }
+    }
+
     public static void main(String[] args) {
         
         B obj = new B();
