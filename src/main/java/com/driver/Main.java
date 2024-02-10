badf0873242aaaa0c7600d8e7b8f93ca65600b8f
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-10 18:35:36 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
 
+class A{
+    public String meth(String str){
+        return str;
+    }
+}
+
+class B extends A{
+    public String meth(String str1){
+        return str1;
+    }
+}
+
 public class Main {
-  
+    public static void main(String[] args) {
+        
+        B obj = new B();
+        String ans = obj.meth("hello");
+        String ans1 = obj1.meth();
+
+    }
 }
