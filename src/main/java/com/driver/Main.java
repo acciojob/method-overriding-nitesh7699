11242aa3ccd396e921a139291aa232a1eae9680e
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-10 19:21:57 +0530

    changes7

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -19,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         
         B obj = new B();
-        String ans = obj.meth("hello");///////
+        String ans = obj.meth("hello");/////
         String ans1 = obj.meth("hello");/////
 
     }
