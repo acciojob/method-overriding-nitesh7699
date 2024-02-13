3776f0584931cf69a276a6e85c347e1bc34996c3
niteshsoni7699 <niteshsoni7699@gmail.com>
2024-02-14 04:13:59 +0530

    latest2 commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,13 +4,13 @@ public class Main {
 
     public static class A{
         public String meth(){
-            return "invoking method from class A";
+            return "Invoking method from class A";
         }
     }
     
     public static class B extends A{//
         public String meth(){
-            return "method is overridden in extended class B";
+            return "Method is overridden in extended class B";
         }
     }
 
